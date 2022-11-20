8e100192da3e4066feb18af35706efeeeecbd93d
cybemachine <cybemachine142@gmail.com>
2022-11-20 18:38:33 +0545

    added config info in log.js

--- commands/log.js
+++ commands/log.js
@@ -2,11 +2,11 @@ const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
   config: {
-    name: "ping",
-    description: "Replies with pong and websocket ping time",
+    name: "log",
+    description: "loggging file for beta release",
   },
   permissions: ["SendMessages"],
-  owner: false,
+  owner: true,
   run: async (client, message, args, config) => {
     console.log(message, args);
   },
