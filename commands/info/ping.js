eb70497b3331071b68308ef646437dcdede25e42
cybemachine <cybemachine142@gmail.com>
2022-11-24 14:10:12 +0545

    added ping command

---
+++ commands/info/ping.js
@@ -0,0 +1,26 @@
+const { Command } = require("reconlx");
+const { MessageEmbed } = require("discord.js");
+const ee = require("../../settings/embed.json");
+
+module.exports = new Command({
+  // options
+  name: "ping",
+  description: `Show Bot Ping`,
+  userPermissions: ["SEND_MESSAGES"],
+  category: "Information",
+  // command start
+  run: async ({ client, interaction, args }) => {
+    interaction.followUp({
+      embeds: [
+        new MessageEmbed()
+          .setColor("Green")
+          .setTitle(`Ping :- ${client.ws.ping}`)
+          .setFooter(
+            "By ridhesh w | cybe",
+            "https://img.icons8.com/color/452/discord-logo.png"
+          ),
+      ],
+      ephemeral: true,
+    });
+  },
+});
