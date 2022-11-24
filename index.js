c07dc37604c8537186cef237a854bfb3b82b138c
cybemachine <cybemachine142@gmail.com>
2022-11-24 17:31:16 +0545

    removed bugs

--- index.js
+++ index.js
@@ -23,24 +23,18 @@ const client = new Client({
 client.commands = {};
 
 // slash handler
-const arrayOfCommands = [];
 fs.readdirSync("./commands").forEach((cmd) => {
   fs.readdirSync(`./commands/${cmd}/`)
     .filter((file) => file.endsWith(".js"))
     .forEach((cmds) => {
       let pull = require(`../commands/${cmd}/${cmds}`);
-      if (!pull.name) return console.log(`${cmds} Command is not Ready`);
+      if (!pull.name || !pull.run)
+        return console.log(`${cmds} Command is not Ready`);
       client.commands[pull.name] = pull;
-      arrayOfCommands.push(pull);
       console.log(`loaded ${pull.name}`);
       if (pull.aliases && Array.isArray(pull.aliases))
         pull.aliases.forEach((alias) => client.aliases.set(alias, name));
     });
-  client.on("ready", async () => {
-    client.guilds.cache.forEach(async (g) => {
-      await client.guilds.cache.get(g.id).commands.set(arrayOfCommands);
-    });
-  });
 });
 
 //music handler
