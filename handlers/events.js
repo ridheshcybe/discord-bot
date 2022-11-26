3404b8f9a871d6ff52bc308d2344546b42fe9934
cybemachine <cybemachine142@gmail.com>
2022-11-26 12:05:34 +0545

    merged

--- handlers/events.js
+++ handlers/events.js
@@ -1,27 +1,100 @@
 const fs = require("fs");
 const chalk = require("chalk");
+const { QuickDB } = require("quick.db");
+const config = require("../../config/config.js");
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+
+const db = new QuickDB();
 
 module.exports = (client) => {
-  console.log("Events Handler:".blue);
+  console.log(chalk.blue("Events Handler:"));
+  client.on("ready", async () => {
+    console.log(
+      "\n" +
+        chalk.greenBright(`[READY] ${client.user.tag} is up and ready to go.`)
+    );
+  });
 
-  fs.readdirSync("./events/").forEach((dir) => {
-    const commands = fs
-      .readdirSync(`./events/${dir}`)
-      .filter((file) => file.endsWith(".js"));
-    for (let file of commands) {
-      let pull = require(`../events/${dir}/${file}`);
-      if (pull.name) {
-        client.events.set(pull.name, pull);
-        console.log(
-          chalk.greenBright(`[HANDLER - EVENTS] Loaded a file: ${pull.name}`)
-        );
-      } else {
-        console.log(
-          chalk.red(
-            `[HANDLER - EVENTS] Couldn't load the file ${file}. missing name or aliases.`
+  client.on("interactionCreate", async (interaction) => {
+    if (interaction.isChatInputCommand()) {
+      const command = client.slash_commands.get(interaction.commandName);
+
+      if (!command) return;
+
+      try {
+        command.run(client, interaction, config, db);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+
+  client.on("messageCreate", async (message) => {
+    if (message.channel.type !== 0) return;
+    if (message.author.bot) return;
+
+    const prefix = "!";
+
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.guild) return;
+    if (!message.member)
+      message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (cmd.length == 0) return;
+
+    let command = client.prefix_commands.get(cmd);
+
+    if (!command) return;
+
+    if (command) {
+      if (command.permissions) {
+        if (
+          !message.member.permissions.has(
+            PermissionsBitField.resolve(command.permissions || [])
           )
-        );
-        continue;
+        )
+          return message.reply({
+            embeds: [
+              new EmbedBuilder()
+                .setDescription(
+                  `🚫 Unfortunately, you are not authorized to use this command.`
+                )
+                .setColor("Red"),
+            ],
+          });
+      }
+
+      if ((command.owner, command.owner == true)) {
+        if (config.Users?.OWNERS) {
+          const allowedUsers = []; // New Array.
+
+          config.Users.OWNERS.forEach((user) => {
+            const fetchedUser = message.guild.members.cache.get(user);
+            if (!fetchedUser) return allowedUsers.push("*Unknown User#0000*");
+            allowedUsers.push(`${fetchedUser.user.tag}`);
+          });
+
+          if (!config.Users.OWNERS.some((ID) => message.member.id.includes(ID)))
+            return message.reply({
+              embeds: [
+                new EmbedBuilder()
+                  .setDescription(
+                    `🚫 Sorry but only owners can use this command! Allowed users:\n**${allowedUsers.join(
+                      ", "
+                    )}**`
+                  )
+                  .setColor("Red"),
+              ],
+            });
+        }
+      }
+
+      try {
+        command.run(client, message, args, prefix, config, db);
+      } catch (error) {
+        console.error(error);
       }
     }
   });
