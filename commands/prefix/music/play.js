0772d5602d4f36b46b8f7f7b0b8bb6c7eb2fa98a
cybemachine <cybemachine142@gmail.com>
2022-11-26 19:08:31 +0545

    added play.js

--- commands/prefix/music/play.js
+++ commands/prefix/music/play.js
@@ -1,75 +1,91 @@
 const { EmbedBuilder } = require("discord.js");
-const music = require("@koenie06/discord.js-music");
 
 module.exports = {
   config: {
     name: "play",
     description: "Plays a song from spotify",
-    usage: "play <URL / TITLE>",
+    usage: "play <URL>",
   },
   permissions: ["SendMessages", "Connect"],
   owner: false,
   alias: ["p", "playsong", "playtrack"],
   run: async (client, message, args, prefix, config, db) => {
-    try {
-      const { channel } = message.member.voice;
-      const text = args.join(" ");
-      if (!channel)
-        return message.reply({
-          embeds: [
-            new EmbedBuilder()
-              .setColor("Red")
-              .setTitle(`❌ ERROR | Please join a Channel first`),
-          ],
-        });
-      if (channel.id !== message.guild.me.voice.channel.id)
-        return message.reply({
-          embeds: [
-            new EmbedBuilder()
-              .setColor("Red")
-              .setTitle(`❌ ERROR | Please join **my** Channel first`)
-              .setDescription(
-                `Channelname: \`${message.guild.me.voice.channel.name}\``
-              ),
-          ],
-        });
-      if (!text)
-        return message.reply({
-          embeds: [
-            new EmbedBuilder()
-              .setColor("Red")
-              .setTitle(`❌ ERROR | You didn't provided a Searchterm`)
-              .setDescription(`Usage: \`${prefix}play <URL / TITLE>\``),
-          ],
-        });
-      message
+    if (!message.member.voice.channel)
+      return message
         .reply({
-          embeds: [
-            new EmbedBuilder()
-              .setColor("Green")
-              .setTitle("Searching Song")
-              .setDescription(`\`\`\`fix\n${text}\n\`\`\``),
-          ],
+          content:
+            ":no_entry_sign: **You must join a voice channel to use that!**",
+          ephemeral: true,
         })
-        .then((msg) =>
-          msg.delete({ timeout: 5000 }).catch((e) => console.log(e.message))
-        );
+        .catch((err) => {
+          console.log(`i couldn't reply to the message: ` + err.message);
+        });
+    if (
+      message.guild.me.voice?.channel &&
+      message.member.voice.channel.id !== message.guild.me.voice.channel.id
+    )
+      return message
+        .reply({
+          content: `:no_entry_sign: You must be listening in **${message.guild.me.voice.channel.name}** to use that!`,
+          ephemeral: true,
+        })
+        .catch((err) => {
+          console.log(`i couldn't reply to the message: ` + err.message);
+        });
+    const songTitle = args.slice(0).join(" ");
 
-      music.play({
-        interaction: interaction,
-        channel: channel,
-        song: text,
-      });
-    } catch (e) {
-      console.log(String(e.stack));
-      return message.reply({
-        embeds: [
-          new EmbedBuilder()
-            .setColor("Red")
-            .setTitle(`❌ ERROR | An error occurred`)
-            .setDescription(`\`\`\`${e.stack}\`\`\``),
-        ],
-      });
+    const queue = await client.player.createQueue(message.guild, {
+      leaveOnEnd: false,
+      leaveOnStop: true,
+      channelEmpty: true,
+      metadata: {
+        channel: message.channel,
+        voice: message.member.voice.channel,
+      },
+    });
+    try {
+      if (!queue.connection) await queue.connect(message.member.voice.channel);
+    } catch {
+      queue.destroy();
+      return await message
+        .reply({
+          content: "**Couldn't join your voice channel!**",
+          ephemeral: true,
+        })
+        .catch((err) => {
+          console.log(`i couldn't reply to the message: ` + err.message);
+        });
     }
+    if (!songTitle)
+      return message
+        .reply({
+          content: `:no_entry_sign: **You should type song name or url.**`,
+          ephemeral: true,
+        })
+        .catch((err) => {
+          console.log(`i couldn't reply to the message: ` + err.message);
+        });
+    message
+      .reply({
+        content: `:watch: Searching ... (\`${songTitle}\`)`,
+        etchReply: true,
+      })
+      .then(async (m) => {
+        const searchResult = await client.player.search(songTitle, {
+          requestedBy: message.author,
+          searchEngine: QueryType.AUTO,
+        });
+        if (!searchResult.tracks.length)
+          return m.edit({ content: `**:mag: Not found.**` });
+        m.edit({
+          content: `:notes: **${searchResult.tracks[0].title}** Added to **Queue** (${searchResult.tracks[0].duration})!`,
+          ephemeral: true,
+        });
+        searchResult.playlist
+          ? queue.addTracks(searchResult.tracks)
+          : queue.addTrack(searchResult.tracks[0]);
+
+        if (!queue.playing) await queue.play();
+      });
   },
 };
--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const {
 } = require("discord.js");
 const chalk = require("chalk");
 const config = require("./config/config");
-const { DisTube } = require("distube");
-const { YtDlpPlugin } = require("@distube/yt-dlp");
-const { SpotifyPlugin } = require("@distube/spotify");
-const { SoundCloudPlugin } = require("@distube/soundcloud");
+const { Player } = require("discord-player");
 
 // Creating a new client:
 const client = new Client({
@@ -40,6 +37,64 @@ const client = new Client({
   },
 });
 
+const player = new Player(client, {
+  ytdlOptions: {
+    quality: "highestaudio",
+    highWaterMark: 1 << 25,
+    requestOptions: {
+      headers: {
+        cookie: "YOUR_YOUTUBE_COOKIE",
+      },
+    },
+  },
+});
+
+player.on("error", (queue, error) => {
+  console.log(`${queue.guild.name} An Error has occurred ${error}`, "error");
+});
+
+player.on("connectionError", (queue, error) => {
+  console.log(`Error emitted from the connection ${error.message}`);
+});
+
+player.on("botDisconnect", (queue) => {
+  console.log(`${queue.guild.name} Disconnected from Channel`);
+});
+
+player.on("connectionCreate", (queue, connection) => {
+  console.log(
+    `${queue.guild.name}: Bot has successfully connected to Voice Channel!`
+  );
+});
+
+player.on("connectionError", (queue, error) => {
+  console.log(
+    `${queue.guild.name}: There has been a connection error, ${error.message}`
+  );
+});
+
+player.on("queueEnd", (queue) => {
+  console.log(`${queue.guild.name}: Queue has finished playing!`);
+});
+
+player.on("trackAdd", (queue, track) => {
+  console.log(`${queue.guild.name}: ${track.title} has been added!`);
+});
+
+player.on("trackEnd", (queue, track) => {
+  console.log(`${queue.guild.name}: ${track.title} has finished playing!`);
+});
+
+player.on("tracksAdd", (queue, tracks) => {
+  console.log(
+    `${queue.guild.name}: A playlist with ${tracks.length} songs has beed added!`
+  );
+});
+
+player.on("trackStart", (queue, track) => {
+  console.log(`${queue.guild.name}: ${track.title} has started playing`);
+});
+
 // Host the bot:
 require("http")
   .createServer((req, res) => res.end("Ready."))
@@ -57,88 +112,13 @@ if (!AuthenticationToken) {
 }
 
 // Handler:
+client.player = player;
 client.events = new Collection();
 client.aliases = new Collection();
 client.user_commands = new Collection();
 client.slash_commands = new Collection();
 client.prefix_commands = new Collection();
 client.message_commands = new Collection();
-client.distube = new DisTube(client, {
-  leaveOnStop: false,
-  emitNewSongOnly: true,
-  emitAddSongWhenCreatingQueue: false,
-  emitAddListWhenCreatingQueue: false,
-  plugins: [
-    new SpotifyPlugin({
-      emitEventsAfterFetching: true,
-    }),
-    new SoundCloudPlugin(),
-    new YtDlpPlugin(),
-  ],
-});
-
-const status = (queue) =>
-  `Volume: \`${queue.volume}%\` | Filter: \`${
-    queue.filter || "Off"
-  }\` | Loop: \`${
-    queue.repeatMode
-      ? queue.repeatMode == 2
-        ? "All Queue"
-        : "This Song"
-      : "Off"
-  }\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
-
-// DisTube event listeners, more in the documentation page
-client.distube
-  //ONCE A SONG STARTS PLAYING SEND INFORMATIONAL MESSAGE
-  .on("playSong", (message, queue, song) =>
-    message.channel.send(
-      `Playing \`${song.name}\` - \`${
-        song.formattedDuration
-      }\`\nRequested by: ${song.user}\n${status(queue)}`
-    )
-  )
-  //ONCE A SONG IS ADDED TO THE QUEUE SEND INFORMATIONAL MESSAGE
-  .on("addSong", (message, queue, song) =>
-    message.channel.send(
-      `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
-    )
-  )
-  //ONCE A PLAYLIST STARTS PLAYING SEND INFORMATIONAL MESSAGE
-  .on("playList", (message, queue, playlist, song) =>
-    message.channel.send(
-      `Play \`${playlist.name}\` playlist (${
-        playlist.songs.length
-      } songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${
-        song.formattedDuration
-      }\`\n${status(queue)}`
-    )
-  )
-  //ONCE A PLAYLIST IS ADDED TO THE QUEUE SEND INFORMATIONAL MESSAGE
-  .on("addList", (message, queue, playlist) =>
-    message.channel.send(
-      `Added \`${playlist.name}\` playlist (${
-        playlist.songs.length
-      } songs) to queue\n${status(queue)}`
-    )
-  )
-  // DisTubeOptions.searchSongs = true
-  .on("searchResult", (message, result) => {
-    let i = 0;
-    message.channel.send(
-      `**Choose an option from below**\n${result
-        .map(
-          (song) => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``
-        )
-        .join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`
-    );
-  })
-  // DisTubeOptions.searchSongs = true
-  .on("searchCancel", (message) => message.channel.send(`Searching canceled`))
-  .on("error", (message, e) => {
-    console.error(e);
-    message.channel.send("An error encountered: " + e);
-  });
 
 ["prefix", "application_commands", "events"].forEach((file) => {
   require(`./handlers/${file}`)(client, config);
