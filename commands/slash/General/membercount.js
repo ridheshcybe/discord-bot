6237bb11e0fd77d0412cd087aa73b5efca9dae16
cybemachine <cybemachine142@gmail.com>
2022-11-28 16:28:45 +0545

    logging

--- commands/slash/General/membercount.js
+++ commands/slash/General/membercount.js
@@ -9,11 +9,13 @@ module.exports = {
     DEFAULT_PERMISSIONS: "SendMessages",
   },
   run: async (client, interaction, config, db) => {
+    console.log(interaction);
     return interaction.reply({
       embeds: [
         new EmbedBuilder()
           .setDescription(
-            client.guilds.cache.get(message.guild.id).memberCount + " members"
+            client.guilds.cache.get(interaction.guild.id).memberCount +
+              " members"
           )
           .setColor("Blue"),
       ],
