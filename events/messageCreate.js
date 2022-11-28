59b98d1ec72df9675328a948ee6935638ddefc96
cybemachine <cybemachine142@gmail.com>
2022-11-28 16:02:17 +0545

    logging

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -18,6 +18,9 @@ module.exports = async (message) => {
   if (!message.member)
     message.member = await message.guild.fetchMember(message);
 
+  const mentions = message.mentions.users;
+  console.log(mentions);
+
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
   if (cmd.length == 0) return;
