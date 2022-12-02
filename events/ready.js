90afc571b192fc8e5800d45b4f4c8632c0ed2d1a
cybemachine <cybemachine142@gmail.com>
2022-12-02 21:11:56 +0545

    testing

--- events/ready.js
+++ events/ready.js
@@ -2,4 +2,6 @@ module.exports = (client, config) => {
   console.log(
     chalk.greenBright(`[READY] ${client.user.tag} is up and ready to go.`)
   );
+  const Guilds = client.guilds.cache.map((guild) => guild.id);
+  console.log(Guilds);
 };
