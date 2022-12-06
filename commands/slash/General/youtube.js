287cb7e355efbe223c6bb30447b35bb222edef74
cybemachine <cybemachine142@gmail.com>
2022-12-06 18:14:48 +0545

    testing

--- commands/slash/General/youtube.js
+++ commands/slash/General/youtube.js
@@ -14,6 +14,7 @@ module.exports = {
   run: async (client, interaction, config, db) => {
     // const data = await pack(interaction.options);
     // interaction.reply(data[0].url);
-    interaction.reply(interaction.options);
+    console.log(interaction.options);
+    interaction.reply("hi");
   },
 };
