3827cdca4e38009d80ca2e2f2cb10d9e077647b2
cybemachine <cybemachine142@gmail.com>
2022-12-06 20:36:38 +0545

    testing

--- commands/slash/General/youtube.js
+++ commands/slash/General/youtube.js
@@ -1,4 +1,6 @@
+const YouTube = require("youtube-node");
 const { SlashCommandBuilder } = require("discord.js");
+const youTube = new YouTube();
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,6 +13,16 @@ module.exports = {
         .setRequired(true)
     ),
   run: async (client, interaction, config, db) => {
+    youTube.setKey(process.env.YOUTUBE_TOKEN);
+
+    youTube.search(
+      interaction.options._hoistedOptions[0].value,
+      1,
+      (error, result) => {
+        if (error) return console.log(error);
+        console.log(JSON.stringify(result, null, 2));
+      }
+    );
     interaction.reply("N.I");
   },
 };
