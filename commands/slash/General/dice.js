40893710ccce97cb6e617afb6a281cee080ee764
cybemachine <cybemachine142@gmail.com>
2022-12-09 10:55:04 +0545

    testing

--- commands/slash/General/dice.js
+++ commands/slash/General/dice.js
@@ -17,7 +17,7 @@ module.exports = {
         .setMaxValue(20)
     ),
   run: async (client, interaction, config, db) => {
-    console.log(interaction.options);
+    console.log(interaction.options.getNumber("d"));
     // execute
     interaction.reply("Testing in progress...");
   },
