425764a391bc769029dbb26310199365c1cd19f6
cybemachine <cybemachine142@gmail.com>
2022-12-10 12:26:04 +0545

    fixed url error

--- commands/slash/web/cat.js
+++ commands/slash/web/cat.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,12 +8,10 @@ module.exports = {
     .setDescription("fetches a cat image from the server")
     .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
   run: async (client, interaction, config, db) => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const body = await res.json();
     interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setColor("green")
-          .setImage("https://api.thecatapi.com/v1/images/search"),
-      ],
+      embeds: [new EmbedBuilder().setColor("green").setImage(body[0].url)],
     });
   },
 };
