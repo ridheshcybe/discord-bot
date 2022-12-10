50906b9d65cc79d55285d00327572ac61a37f7c6
cybemachine <cybemachine142@gmail.com>
2022-12-10 13:40:55 +0545

    added dog command

---
+++ commands/slash/fun/dog.js
@@ -0,0 +1,21 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  PermissionFlagsBits,
+} = require("discord.js");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("dog")
+    .setDescription("fetches a dog image from the server")
+    .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
+  run: async (client, interaction, config, db) => {
+    const res = await fetch("https://api.thedogapi.com/v1/images/search");
+    const body = await res.json();
+    interaction.reply({
+      embeds: [new EmbedBuilder().setColor("Green").setImage(body[0].url)],
+    });
+  },
+};
