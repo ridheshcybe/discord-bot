1082cd341d00ebb80c820095ca49b8d7285d695b
cybemachine <cybemachine142@gmail.com>
2022-12-10 12:30:05 +0545

    fixed error in template

--- templates/slash_command.js
+++ templates/slash_command.js
@@ -1,4 +1,8 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  PermissionFlagsBits,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
