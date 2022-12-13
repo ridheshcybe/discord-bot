69f66dd562d2e220287f46b2bc9e4c0e79584858
cybemachine <cybemachine142@gmail.com>
2022-12-13 17:08:31 +0545

    added github

--- commands/slash/web/github.js
+++ commands/slash/web/github.js
@@ -3,13 +3,52 @@ const {
   EmbedBuilder,
   PermissionFlagsBits,
 } = require("discord.js");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("github")
     .setDescription("gets github information")
-    .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
+    .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
+    .addStringOption((option) =>
+      option
+        .setName("username")
+        .setDescription(
+          "username of the github account you want to get information about"
+        )
+        .setRequired(true)
+    ),
   run: async (client, interaction, config, db) => {
-    interaction.reply("Not Implemened");
+    const username = interaction.options.getString("username");
+    const fetched = await fetch("https://api.github.com/users/" + username, {
+      headers: {
+        "User-Agent": "User-" + username,
+      },
+    });
+    const body = await fetched.json();
+
+    if (body.message && body.message == "Not Found")
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder().setColor("Red").setDescription("User not found"),
+        ],
+      });
+
+    interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Green")
+          .setTitle(`Github - ${body.name}`)
+          .setFields(
+            { name: "followers", value: body.followers || 0 },
+            { name: "public repos", value: body.public_repos || 0 },
+            { name: "following", value: body.following || 0 },
+            { name: "bio", value: body.bio || "None" },
+            { name: "blog", value: body.blog || "None" },
+            { name: "location", value: body.location || "None" }
+          ),
+      ],
+    });
   },
 };
